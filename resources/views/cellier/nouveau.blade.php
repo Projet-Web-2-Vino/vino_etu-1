78312eb49c34fe5632880e231c023535904c83fd
Annabelle <annabellegamache@gmail.com>
2023-01-26 10:03:53 -0500

    formulaire modifier cellier stylisé

--- resources/views/cellier/nouveau.blade.php
+++ resources/views/cellier/nouveau.blade.php
@@ -7,10 +7,10 @@
 <form action="{{ route('cellier.creer')}}" method="POST">
     {{-- Token pour la securiter du formulaire --}}
     @csrf
-    <div class="bg-gray-100  flex flex-col justify-center ">
-        <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div class="relative px-4 py-10 bg-white mx-8 md:mx-0 shadow rounded-3xl sm:p-10">
-                <div class="max-w-md mx-auto">
+    <div class="bg-gray-100 p-10 flex flex-col  min-h-screen">
+        
+            <div class="relative px-4 py-10 bg-white">
+                <div class="max-w-full mx-auto">
                     <div class="flex items-center space-x-5">
                         <div class="h-12 w-12 rounded-full justify-center items-center text-yellow-500 text-2xl font-mono">
                             <img src="https://static.thenounproject.com/png/5003274-200.png" alt="">
@@ -23,7 +23,7 @@
                     <div class="divide-y divide-gray-200">
                         <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                         <div class="flex flex-col">
-                            <label class="leading-loose">Nom Cellier</label>
+                            <label class="leading-loose">Nom de votre cellier</label>
                             {{-- Section pour le nom du cellier --}}
                             <input class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" name="nom_cellier" type="text" value="{{ old('nom_cellier')}}" />
                             @error('nom_cellier')
@@ -51,10 +51,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        
     </div>
-    @if (session()->has('success'))
-    <span style="color:green">{{ session('success') }}</span>
-    @endif
 </form>
 @endsection
