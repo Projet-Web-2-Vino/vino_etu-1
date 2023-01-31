69ca7e6ee08af03260532123b2136630bcdbdb1b
Annabelle <annabellegamache@gmail.com>
2023-01-31 16:19:47 -0500

    harmonisation formulaire ajout/edit cellier

--- resources/views/cellier/edit.blade.php
+++ resources/views/cellier/edit.blade.php
@@ -4,13 +4,13 @@
 <form action="{{ route('cellier.update', ['id' => $cellier->id])}}" method="POST">
     {{-- Token pour la securiter du formulaire --}}
     @csrf
-            <div class="relative px-4 py-10 bg-white">
+            <div class="relative p-4 bg-white">
                 <div class="max-w-full mx-auto">
-                    <div class="flex items-center space-x-5">
-                            <div class="h-12 w-12 rounded-full justify-center items-center text-yellow-500 text-2xl font-mono">
+                    <div class="flex items-center space-x-3">
+                            <div class="w-12 mr-1">
                                 <img src="https://static.thenounproject.com/png/5003274-200.png" alt="">
                             </div>
-                            
+                            <h2 class="text-xl">Modification cellier</h2>
                         </div>
                         <div class="divide-y divide-gray-200">
                             <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
@@ -23,19 +23,14 @@
                                 @enderror
                                 <input name="id_usager" type="hidden" value="{{Auth::id()}}" />
                             </div>
-                            <!--
-                            <div class="flex flex-col">
-                                <label class="leading-loose">Description</label>
-                                <input type="text" class="px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" >
+            
                             </div>
-                        -->
-                            </div>
-                            <div class="pt-4 flex items-center space-x-4">
-                                <a class="flex justify-center items-center w-full text-gray-900 px-4 py-3 rounded-md focus:outline-none" href='/cellier'>
+                            <div class="pt-4 flex flex-wrap justify-end space-x-4">
+                                <a class="flex  justify-center items-center  text-gray-900 px-4 py-3 rounded-md focus:outline-none" href='/cellier'>
                                 <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg> Annuler
                                 </a>
                             
-                                <button class="bg-red-800 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none">Modifier</button>
+                                <button class="bg-slate-900 flex justify-center items-center  text-white px-4 py-3 rounded-md">Modifier</button>
                                 
                             </div>
                         </div>
