62eb537421688d63d4d33a80d5bdb647facc29f8
Annabelle <annabellegamache@gmail.com>
2023-02-01 16:34:04 -0500

    modification de la table commentaire en note

--- database/sql/vinodb.sql
+++ database/sql/vinodb.sql
@@ -56,11 +56,11 @@ ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
 -- -----------------------------------------------------
 -- Table `vinodb`.`vino__commentaire`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `vino__commentaire`;
+DROP TABLE IF EXISTS `vino__note`;
 CREATE TABLE `vino__commentaire` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `id_usager` INT(20) NULL,
-  `commentaire` VARCHAR(200) NULL,
+  `note` INT(11) NULL,
   `id_bouteille` INT NULL,
   PRIMARY KEY (`id`))
 ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
