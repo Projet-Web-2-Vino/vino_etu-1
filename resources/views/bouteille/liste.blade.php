03f42d6acd3fe049afb38fba649e1e9527518134
Fabrizio <69865037+fabiobrizzi@users.noreply.github.com>
2023-02-06 13:41:33 -0500

    Regler bug sur le right click de la description

--- resources/views/bouteille/liste.blade.php
+++ resources/views/bouteille/liste.blade.php
@@ -85,21 +85,24 @@
             </button> --}}
 
             {{-- Click pour Info description sur la bouteille ALPINEJS--}}
-            <div x-data="{showContextMenu:false}">
-                <div class="relative" @click.away="showContextMenu=false">
-                    <button class="bg-white h-10 w-10 leading-10 text-center text-gray-800 text-xl shadow-md border border-gray-200 hover:border-gray-300 focus:border-gray-300 rounded-lg transition-all font-semibold outline-none focus:outline-none" x-on:contextmenu="$event.preventDefault();showContextMenu=true" @click.prevent="showContextMenu=false">
-                        <i class="fas fa-info"></i>
-                    </button>
-                    <div class="absolute mt-12 top-0 left-1 min-w-full w-48 z-30" style="display:none;" x-show="showContextMenu" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
-                        <span class="absolute top-0 left-0 w-2 h-2 bg-white transform rotate-45 -mt-1 ml-3 border-gray-300 border-l border-t z-20"></span>
-                        <div class="bg-white overflow-auto rounded-lg shadow-md w-full relative z-10 py-2 border border-gray-300 text-gray-800 text-xs">
-                            <div class="p-1">
-                                <small><p>description</p></small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div x-data="{showContextMenu:false}">
+  {{-- <div class="relative" @click.away="showContextMenu=false"> --}}
+  <div class="relative" @click.away="showContextMenu=true">
+    <button class="bg-white h-10 w-10 leading-10 text-center text-gray-800 text-xl shadow-md border border-gray-200 hover:border-gray-300 focus:border-gray-300 rounded-lg transition-all font-semibold outline-none focus:outline-none" @click="showContextMenu=true" @click.prevent="showContextMenu=false">
+      <i class="fas fa-info"></i>
+    </button>
+    <div class="absolute mt-12 top-0 left-1 min-w-full w-48 z-30" style="display:none;" x-show="showContextMenu" x-transition:enter="transition ease duration-100 transform" x-transition:enter-start="opacity-0 scale-90 translate-y-1" x-transition:enter-end="opacity-100 scale-100 translate-y-0" x-transition:leave="transition ease duration-100 transform" x-transition:leave-start="opacity-100 scale-100 translate-y-0" x-transition:leave-end="opacity-0 scale-90 translate-y-1">
+      <span class="absolute top-0 left-0 w-2 h-2 bg-white transform rotate-45 -mt-1 ml-3 border-gray-300 border-l border-t z-20"></span>
+      <div class="bg-white overflow-auto rounded-lg shadow-md w-full relative z-10 py-2 border border-gray-300 text-gray-800 text-xs">
+        <div class="p-1">
+          <small><p>description</p></small>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+            {{-- Section Autre --}}
 
 
           </div>
