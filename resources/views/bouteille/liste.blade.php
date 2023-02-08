c37958097dc99041b8e871260a4435941bdb5026
Fabrizio <69865037+fabiobrizzi@users.noreply.github.com>
2023-02-08 11:53:35 -0500

    centre les filtres

--- resources/views/bouteille/liste.blade.php
+++ resources/views/bouteille/liste.blade.php
@@ -48,66 +48,66 @@
 
 
         {{-- Section filtre --}}
-        <div class="mb-3">
-        {{--Par type--}}
-        <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-type">Type<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-        <!-- Dropdown menu -->
-        <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-type">
+        <div class="mb-3 flex justify-center space-x-2">
+            {{--Par type--}}
+            <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-type">Type<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+            <!-- Dropdown menu -->
+            <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-type">
 
-            <ul class="py-1" aria-labelledby="dropdown-type">
+                <ul class="py-1" aria-labelledby="dropdown-type">
+                    <li>
+                        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 1] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Rouge</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 2] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Blanc</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 3] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Rosé</a>
+                    </li>
+                </ul>
+            </div>
+
+            {{--Par pays--}}
+            <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-pays">Provenance<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+            <!-- Dropdown menu -->
+            <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-pays">
+                <ul class="py-1" aria-labelledby="dropdown-pays">
+                    @foreach ($pays as $data)
+                    <li>
+                        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'pays' => $data->pays] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">{{$data->pays}}</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+
+
+        {{--Par préférence--}}
+        <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-note">Note<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+        <!-- Dropdown menu -->
+        <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-note">
+
+            <ul class="py-1" aria-labelledby="dropdown-note">
                 <li>
-                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 1] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Rouge</a>
+                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 1] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">1<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
                 </li>
                 <li>
-                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 2] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Blanc</a>
+                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 2] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">2<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
                 </li>
                 <li>
-                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'type' => 3] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Rosé</a>
+                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 3] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">3<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
+                </li>
+                <li>
+                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 4] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">4<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
+                </li>
+                <li>
+                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 5] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">5<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
                 </li>
             </ul>
         </div>
-
-        {{--Par pays--}}
-        <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-pays">Provenance<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-        <!-- Dropdown menu -->
-        <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-pays">
-            <ul class="py-1" aria-labelledby="dropdown-pays">
-                @foreach ($pays as $data)
-                <li>
-                    <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'pays' => $data->pays] ) }}" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">{{$data->pays}}</a>
-                </li>
-                @endforeach
-            </ul>
-        </div>
+        <a class="ml-3 text-white bg-neutral-600 font-medium rounded text-lg px-3 py-2 text-center inline-flex items-center" href="{{ route('bouteille.liste', ['id' => $cellier->id] ) }}"><i class="fa-solid fa-xmark"></i></a>
 
 
-    {{--Par préférence--}}
-    <button class="text-white bg-slate-700 font-medium rounded text-sm p-2 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown-note">Note<svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-    <!-- Dropdown menu -->
-    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4" id="dropdown-note">
-
-    <ul class="py-1" aria-labelledby="dropdown-note">
-    <li>
-        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 1] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">1<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
-    </li>
-    <li>
-        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 2] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">2<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
-    </li>
-    <li>
-        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 3] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">3<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
-    </li>
-    <li>
-        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 4] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">4<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
-    </li>
-    <li>
-        <a href="{{ route('bouteille.liste', ['id' => $cellier->id,'note' => 5] ) }}" class="flex text-sm hover:bg-gray-100 text-gray-700  px-4 py-2">5<svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg></a>
-    </li>
-    </ul>
     </div>
-    <a class="ml-3 text-white bg-neutral-600 font-medium rounded text-lg px-3 py-2 text-center inline-flex items-center" href="{{ route('bouteille.liste', ['id' => $cellier->id] ) }}"><i class="fa-solid fa-xmark"></i></a>
-
-
-</div>
 
 
     <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
@@ -151,7 +151,7 @@
         </div>
         {{-- FIN Section Action  --}}
 
-        {{-- Section img selon bouteille selectionner  --}}
+        {{-- Section img  --}}
         <div class='zoneImg '>
                 @switch($info->type)
                 @case(1)
@@ -181,14 +181,14 @@
             <div x-data="{showContextMenu:false}">
                 {{-- ContextMenu False / Modale ferme quand on click away --}}
                 <div class=" relative" @click.away="showContextMenu=false">
-                    <button class=" h-10 w-10 text-center text-gray-800 text-xl  rounded-lg  font-semibold outline-none" @click="showContextMenu=true" " >
+                    <button class=" h-10 w-10 leading-10 text-center text-gray-800 text-xl  rounded-lg  font-semibold outline-none" @click="showContextMenu=true" " >
                         <i class="fa-solid fa-circle-info"></i>
                     </button>
                   <div class="z-50 absolute mt-12 bottom-0 right-0 min-w-full w-25 " style="display:none;" x-show="showContextMenu" >
                     <div class="description z-50 p-5 bg-white overflow-auto rounded-lg shadow-md w-full relative py-2 border border-gray-300 text-gray-800 text-2xl">
 
                         <p class="text-xs"><small><strong>Pays </strong>{{ $info->pays}}</small></p>
-                        <p class="flex  text-xs"><small><strong>Prix: </strong>{{ $info->prix_saq}} $</small></p>
+                        <p class="flex  text-xs"><small><strong>Prix: </strong>{{ $info->prix_saq}}$</small></p>
                         <p class="flex  text-xs"><small><strong>Code Saq: </strong>{{ $info->code_saq}}</small></p>
                     </div>
                   </div>
